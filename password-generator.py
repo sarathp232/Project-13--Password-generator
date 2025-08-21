f7d38efd2afbb81972731304683302e90e71355d
sarathp232 <sarathp232@gmail.com>
2025-08-21 20:50:07 +0530

    Password generator using python with letters, characters and number

---
+++ password-generator.py
@@ -0,0 +1,47 @@
+import random
+import string
+
+def generate_password(min_length,numbers=True,special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    pwd = ''
+
+    meets_criteria = False
+    has_numbers = False
+    has_special = False
+
+    while not meets_criteria or len(pwd)< min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_numbers = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+
+        if numbers:
+            meets_criteria = has_numbers
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd
+
+pwd  = generate_password(10)
+
+min_length =int(input("Enter the minimum length: "))
+has_numbers = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_special = input("Do you want to have special characters (y/n)? ").lower() == 'y'
+
+pwd = generate_password(min_length,has_numbers,has_special)
+
+print(f"The password is {pwd}")
